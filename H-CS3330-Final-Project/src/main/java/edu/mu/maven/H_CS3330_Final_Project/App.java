aa5a7d3ab9f004a2d5f3c37a4d9f69225948a329
Henry Clark <hry1776@gmail.com>
2024-04-30 18:50:04 -0500

    Created Maven Project.

---
+++ H-CS3330-Final-Project/src/main/java/edu/mu/maven/H_CS3330_Final_Project/App.java
@@ -0,0 +1,13 @@
+package edu.mu.maven.H_CS3330_Final_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
