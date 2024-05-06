ad9e59a057b7a05049bae01c89b9b4d9cefd4d67
Kyle Blank <kcgblank@gmail.com>
2024-05-06 18:24:09 -0500

    edited location classes, added in methods to apply modifiers
    (right now they all only adjust dexterity)

--- H-CS3330-Final-Project/src/main/java/edu/mu/locations/Mountain.java
+++ H-CS3330-Final-Project/src/main/java/edu/mu/locations/Mountain.java
@@ -1,5 +1,47 @@
 package edu.mu.locations;
 
+import edu.mu.characters.PlayerCharacter;
+
 public class Mountain extends LocationSuper {
+
+	protected int strengthMod;
+	protected int dexMod;
+	protected int constitutionMod;
+	protected int IntelligenceMod;
+	protected int wisdomMod;
+	protected int charismaMod;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	public void Apply(PlayerCharacter character) {
+		statsBase = character.getStats();
+		statsAltered = character.getStats();
+		if (statsAltered.containsKey("Dexterity")) {
+            int currentDex = statsAltered.get("Dexterity");
+            statsAltered.put("Dexterity", currentDex - 1);
+            character.setStats(statsAltered);
+            
+        }
+		
+		
+	}
+
+	
+	public void Depart(PlayerCharacter character) {
+		character.setStats(statsBase);
+	
+	}
+	
+	
 
 }
